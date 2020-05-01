6c6507c5a6668b92185dcd41da78e801d11ac553
joshbarrientos1 <joshbarrientos1@gmail.com>
2020-05-01 17:53:59 -0400

    added Luhans changes

--- findings.php
+++ findings.php
@@ -48,10 +48,12 @@
 <div class="container" style="padding-top: 40px;">
 <form method="post">
   <div class="form-group" style="padding-left: 5%">
-    <label style="color:azure" for="formGroupExampleInput">Here we have a interactive graph produced by Luhan. Enter the number of outside people a person conacts a day.(This can be a decimal) Press Enter when ready.</label>
+    <label style="color:azure" for="formGroupExampleInput">Here we have a interactive graph produced by Luhan that covers patern predictions after 4/12. Enter the number of outside people a person conacts a day.(This can be a decimal) Press Enter when ready.</label>
         <div style="width: 160px">
             <input type="text" name = "rate" class="form-control" placeholder="Number of People" aria-label="Username" aria-describedby="basic-addon1">
         </div>
+        <div>
+</div>
     </div>
 </form>
 <div style="padding-left: 5%; padding-right: 5%; padding-bottom 200 px; padding-right: 5%">
@@ -64,8 +66,9 @@
         echo '<img src="'.$python.'"/>';
 ?>
 
+<span class="label label-success"style="color:azure"><a href="https://towardsdatascience.com/infectious-disease-modelling-beyond-the-basic-sir-model-216369c584c4">Click here for more information regarding the graph</a></span>
 
-    </div>
+</div>
 <div>
     <div class="container" style="padding-top: 20px; padding-left: 20%; padding-bottom 200 px; padding-right: 20%">
         <!-- <h3 class="text-center" style="color:azure">This is a graph created by Tim through the use of a python script!</h3> -->
@@ -80,12 +83,37 @@
     <div class="container" style="padding-top: 20px; padding-left: 20%; padding-bottom 200 px; padding-right: 20%">
         <!-- <h3 class="text-center" style="color:azure">This is a graph created by Tim through the use of a python script!</h3> -->
         <div class="center-text">
-            <label style="color:azure" for="formGroupExampleInput">This is a spreadshit creadt by Chaewon. She concluded from the spreadsheet that the average infected ages are around 55-56 based on the frequency</label>
+            <label style="color:azure" for="formGroupExampleInput">This is a spreadsheet created by Chaewon. She concluded from the spreadsheet that the average infected ages are around 55-56 based on the frequency</label>
         </div>
         <img src='images/chun-stat.png' width="100%">
     </div>
 </div>
 
-<div style="height: 50px"></div>
+<div style="height: 50px">
+</div>
+<div style="height: 50px">
+    <span class="label label-success" style="color:azure">Sources</span>
+</div>
+<div>
+    <span class="label label-success"style="color:azure"><a href="https://coronavirus.1point3acres.com/en">https://coronavirus.1point3acres.com/en</a></span>
+</div>
+<div>
+    <span class="label label-success"style="color:azure"><a href="https://www.chwsny.org/wp-content/uploads/2018/04/Full_CHWS_NY_Tracking_Report-2018b-1.pdf">https://www.chwsny.org/wp-content/uploads/2018/04/Full_CHWS_NY_Tracking_Report-2018b-1.pdf</a></span>
+</div>
+<div>
+    <span class="label label-success"style="color:azure"><a href="https://www.census.gov/quickfacts/eriecountynewyork">https://www.census.gov/quickfacts/eriecountynewyork</a></span>
+</div>
+<div>
+    <span class="label label-success"style="color:azure"><a href="https://patient.info/news-and-features/coronavirus-how-quickly-do-covid-19-symptoms-develop-and-how-long-do-they-last">https://patient.info/news-and-features/coronavirus-how-quickly-do-covid-19-symptoms-develop-and-how-long-do-they-last</a></span>
+</div>
+<div>
+    <span class="label label-success"style="color:azure"><a href="https://erieny.maps.arcgis.com/apps/opsdashboard/index.html#/dd7f1c0c352e4192ab162a1dfadc58e1">https://erieny.maps.arcgis.com/apps/opsdashboard/index.html#/dd7f1c0c352e4192ab162a1dfadc58e1</a></span>
+</div>
+<div>
+    <span class="label label-success"style="color:azure"><a href="https://www.wivb.com/health/coronavirus/per-erie-county-data-more-men-dying-from-covid-19-than-women/">https://www.wivb.com/health/coronavirus/per-erie-county-data-more-men-dying-from-covid-19-than-women/</a></span>
+</div>
+
+<div style="height: 50px">
+</div>
 </body>
 </html>
